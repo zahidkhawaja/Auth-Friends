bd2980928400739a6d7a095004b76aede537c4f5
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-24 16:23:58 -0400

    Render friends from API on dashboard

--- friends/src/components/Dashboard.js
+++ friends/src/components/Dashboard.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Dashboard = () => {
+    const [friendsData, setFriendsData] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get("/api/friends")
+        .then(res => {
+            console.log(res.data);
+            setFriendsData(res.data);
+        })
+    }, [])
+
     return (
         <div className = "dashboard">
             <h2>Dashboard</h2>
+            <h3>Friends</h3>
+            {friendsData.map(friend => (
+                <div id = {friend.id}>
+                    {friend.name}
+                </div>
+            ))}
         </div>
     );
 };

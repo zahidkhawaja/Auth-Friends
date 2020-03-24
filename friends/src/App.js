57cc5b1452efc97a0c10f337752978dadb360dbb
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-24 15:53:34 -0400

    Add routing, homepage

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import './App.css';
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-       <h1>Auth Friends</h1>
+       <nav className = "navigation">
+         <Link to = "/">Home</Link>
+         <Link to = "/login">Login</Link>
+         <Link to = "/dashboard">Dashboard</Link>
+       </nav>
       </header>
-      <Login />
+      <Switch>
+
+        <Route exact path = "/">
+          <Home/>
+        </Route>
+
+        <Route exact path = "/login">
+          <Login/>
+        </Route>
+
+        <Route exact path = "/dashboard">
+          <Dashboard/>
+        </Route>
+
+      </Switch>
     </div>
+    </Router>
   );
 }
 

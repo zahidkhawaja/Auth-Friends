db9cbc4b2d2deb76994329f3d72de9c65ec5205c
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-24 16:14:20 -0400

    Add PrivateRoute for Dashboard

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -26,9 +27,7 @@ function App() {
           <Login/>
         </Route>
 
-        <Route exact path = "/dashboard">
-          <Dashboard/>
-        </Route>
+        <PrivateRoute exact path = "/dashboard" component = {Dashboard} />
 
       </Switch>
     </div>

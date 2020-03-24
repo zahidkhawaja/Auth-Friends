5734a827566ad87c3c3809eaa49c56b8ba5aead8
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-24 16:30:18 -0400

    Dashboard styling

--- friends/src/components/Dashboard.js
+++ friends/src/components/Dashboard.js
@@ -16,12 +16,15 @@ const Dashboard = () => {
     return (
         <div className = "dashboard">
             <h2>Dashboard</h2>
-            <h3>Friends</h3>
+            <div className = "allfriends">
             {friendsData.map(friend => (
-                <div id = {friend.id}>
-                    {friend.name}
+                <div id = {friend.id} className = "friend">
+                    <h3>{friend.name}</h3>
+                    <p>Age: {friend.age}</p>
+                    <p>Email: {friend.email}</p>
                 </div>
             ))}
+        </div>
         </div>
     );
 };

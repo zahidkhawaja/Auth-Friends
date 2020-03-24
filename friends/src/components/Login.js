b605f96d6e5e794b187d44354cd89aaa3e4e2abe
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-24 16:05:33 -0400

    Add login functionality (post data), set token in LS, and redirect to dashboard

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
+    let history = useHistory();
     const [loginData, setLoginData] = useState({
         username: "",
         password: ""
@@ -15,6 +18,17 @@ const Login = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
+        axiosWithAuth()
+        .post("/api/login", loginData)
+        .then(res => {
+            console.log(res);
+            window.localStorage.setItem("token", JSON.stringify(res.data.payload));
+            history.push("/dashboard");
+
+        })
+        .catch(err => {
+            console.log(err);
+        })
         setLoginData({
             username: "",
             password: ""
